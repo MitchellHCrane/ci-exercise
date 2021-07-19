2c4c73fd14fb4418203d5e8ae4498f921ef9eb65
Mitchell Crane <mitchellhcrane@gmail.com>
2021-07-19 11:54:57 -0700

    second test added

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-const getGreeting = require('./project');
+const getGreeting = require("./project");
 
-test('can create a basic greeting', () => {
-  expect(getGreeting('Sam')).toBe("Hi Sam");
+test("can create a basic greeting", () => {
+  expect(getGreeting("Sam")).toBe("Hi Sam");
+});
+
+test("can create a basic greeting 2", () => {
+  expect(getGreeting("Mitch")).toBe("Hi Mitch");
 });
